0d932b79c2f3ead1f94f45a0f11f8e9d47459ef7
GustavoCabelo <gustavo.s.cabelo@gmail.com>
2018-01-16 22:10:00 +0100

    Update 17/01/2018

--- Sentimental-Analysis.R
+++ Sentimental-Analysis.R
@@ -8,6 +8,7 @@ library(ggplot2)
 library(magrittr)
 library(rattle)
 library(data.table)
+library(dplyr)
 
 #----------------- Set Path ------------------------------------------
 
@@ -17,7 +18,8 @@ path <- "D:/Profissional/Ubiqum/Data Analytics - Understanding customers/Program
 #     Source: crawl-data/CC-MAIN-2017-51 (0000 to 0100 paths)---- 
 #     Large Matrix sentiment  
 #---- iPhone ----
-iPhone <- read.csv(paste0(path,"/Task 3/1-Large Matrixes/iPhoneLargeMatrix.csv"), sep = ";")  
+iPhoneOriginal <- read.csv(paste0(path,"/Task 3/1-Large Matrixes/iPhoneLargeMatrix.csv"), sep = ";")  
+iPhone <- iPhoneOriginal
 
 # Feature selection for relevant attributes for iPhone----
 {
@@ -33,11 +35,11 @@ iPhone <- read.csv(paste0(path,"/Task 3/1-Large Matrixes/iPhoneLargeMatrix.csv")
 
 # Overview ----
 length(iPhone) 
-      # 16
+# 16
 str(iPhone)
-      # nrows: 5426   
-      # dependent variable: $iphoneSentiment
-      # attr types: int
+# nrows: 5426   
+# dependent variable: $iphoneSentiment
+# attr types: int
 summary(iPhone)
 # $iphoneSentiment: 16 NA's
 # id           iphone         iphonecampos      iphonecamneg       iphonecamunc      iphonedispos    
@@ -66,91 +68,77 @@ summary(iPhone)
 # NA's   :16  
 
 histogram(iPhone$iphoneSentiment)
+table(iPhone$iphoneSentiment)
+# 4172 of the cases returning as "0" = 77,11%
 
-# Save clean subset----
-#write.csv(iPhone, paste0(path,"/Task 3/1-Large Matrixes/iPhoneonly.csv"))
-iPhone <- read.csv(paste0(path,"/Task 3/1-Large Matrixes/iPhoneonly.csv"))
-iPhone$X <- NULL
-#---- Galaxy ----
-Galaxy <- read.csv(paste0(path,"/Task 3/1-Large Matrixes/GalaxyLargeMatrix.csv"), sep = ";")  
-
-# Feature selection for relevant attributes for Galaxy----
+# Correlation between attributes
 {
-  a <- colnames(Galaxy[,which(colnames(Galaxy) %like% "samsung")]) #<--- vector with columns name that contain "Galaxy"
-  b <- colnames(Galaxy[,which(colnames(Galaxy) %like% "google")]) #<--- vector with columns name that contain "ios"
-  c <- colnames(Galaxy[,which(colnames(Galaxy) %like% "galaxy")]) #<--- vector with columns name that contain "ios"
-  
-  
-  Galaxy <-  Galaxy[colnames(Galaxy[,c("id",a,b,"galaxySentiment")])] #<--- Create Galaxy subset filtered with attributes selected
-  rm(a,b,c)
-  
-}
-
-# Overview ----
-length(Galaxy) # 16
-str(Galaxy)
-# nrows: 5426
-# dependent variable: $galaxySentiment
-# attr types: int
-summary(Galaxy)
-# $iphoneSentiment: 16 NA's
-
-# id       samsunggalaxy      samsungcampos      samsungcamneg      samsungcamunc      samsungdispos    
-# Min.   :   0   Min.   : 0.00000   Min.   : 0.00000   Min.   : 0.00000   Min.   : 0.00000   Min.   : 0.0000  
-# 1st Qu.:1356   1st Qu.: 0.00000   1st Qu.: 0.00000   1st Qu.: 0.00000   1st Qu.: 0.00000   1st Qu.: 0.0000  
-# Median :2712   Median : 0.00000   Median : 0.00000   Median : 0.00000   Median : 0.00000   Median : 0.0000  
-# Mean   :2712   Mean   : 0.08994   Mean   : 0.07667   Mean   : 0.02709   Mean   : 0.02635   Mean   : 0.1476  
-# 3rd Qu.:4069   3rd Qu.: 0.00000   3rd Qu.: 0.00000   3rd Qu.: 0.00000   3rd Qu.: 0.00000   3rd Qu.: 0.0000  
-# Max.   :5425   Max.   :11.00000   Max.   :16.00000   Max.   :16.00000   Max.   :14.00000   Max.   :17.0000  
-# 
-# samsungdisneg      samsungdisunc      samsungperpos     samsungperneg     samsungperunc      googleandroid    
-# Min.   : 0.00000   Min.   : 0.00000   Min.   : 0.0000   Min.   : 0.0000   Min.   : 0.00000   Min.   :0.00000  
-# 1st Qu.: 0.00000   1st Qu.: 0.00000   1st Qu.: 0.0000   1st Qu.: 0.0000   1st Qu.: 0.00000   1st Qu.:0.00000  
-# Median : 0.00000   Median : 0.00000   Median : 0.0000   Median : 0.0000   Median : 0.00000   Median :0.00000  
-# Mean   : 0.07354   Mean   : 0.05547   Mean   : 0.1574   Mean   : 0.1052   Mean   : 0.08883   Mean   :0.08367  
-# 3rd Qu.: 0.00000   3rd Qu.: 0.00000   3rd Qu.: 0.0000   3rd Qu.: 0.0000   3rd Qu.: 0.00000   3rd Qu.:0.00000  
-# Max.   :18.00000   Max.   :15.00000   Max.   :34.0000   Max.   :23.0000   Max.   :26.00000   Max.   :8.00000  
-# 
-# googleperpos      googleperneg       googleperunc      galaxySentiment  
-# Min.   : 0.0000   Min.   : 0.00000   Min.   : 0.00000   Min.   :-81.000  
-# 1st Qu.: 0.0000   1st Qu.: 0.00000   1st Qu.: 0.00000   1st Qu.:  0.000  
-# Median : 0.0000   Median : 0.00000   Median : 0.00000   Median :  0.000  
-# Mean   : 0.1576   Mean   : 0.09307   Mean   : 0.08238   Mean   :  2.242  
-# 3rd Qu.: 0.0000   3rd Qu.: 0.00000   3rd Qu.: 0.00000   3rd Qu.:  0.000  
-# Max.   :23.0000   Max.   :23.00000   Max.   :21.00000   Max.   :207.000  
-# NA's   :16     
-# Save clean subset----
-write.csv(Galaxy, paste0(path,"/Task 3/1-Large Matrixes/Galaxyonly.csv"))
-
-############# Pre-process ########################################
-#-----------> Remove NAs----
-#---- Backup NAs----
-# IphoneNAs <- iPhone[which(is.na(iPhone$iphoneSentiment)),]
-# saveRDS(IphoneNAs, paste0(path,"/Task 3/1-Large Matrixes/IphoneNAs.rds"))
-#---- NAs Omit ----
-iPhone <- na.omit(iPhone)
-
-
-#--------------- Scale iphoneSentiment from 0 to Max() ------
-iPhone$iphoneSentiment <- iPhone$iphoneSentiment+(min(iPhone$iphoneSentiment)*-1)
-
-# Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
-# 0.0   271.0   271.0   273.4   271.0   623.0 
-
-#--------------- Scale rating ranges from 1 to 10 ------
-Scale <- function(x) {
-  return(x/(max(x))) #<---- divide the value for its maximum (column)
-}
-
-#--------------- Normalize Waps intensity  ------
-iPhone$iphoneSentiment <- apply(iPhone[,(ncol(iPhone))], 2, FUN = "Scale") #<----- normalize by ROW to weight higher the max values by row
-
-#Finding Correlation between attributes
-{
-  
   corrData <- cor(iPhone)
   corrData
   corrplot(corrData)
 }
-## Cross Validation method set
-CrossValidation <- trainControl(method = "repeatedcv", number = 4, repeats = 1)
+
+# Save clean subset----
+# write.csv(iPhone, paste0(path,"/Task 3/1-Large Matrixes/iPhoneonly.csv"), row.names=FALSE)
+iPhone <- read.csv(paste0(path,"/Task 3/1-Large Matrixes/iPhoneonly.csv"))
+
+
+###################### Pre-process #############################
+#### Remove NAs ####
+#---- Backup NAs----
+# IphoneNAs <- iPhone[which(is.na(iPhone$iphoneSentiment)),]
+# saveRDS(IphoneNAs, paste0(path,"/Task 3/1-Large Matrixes/IphoneNAs.rds"))
+
+#---- NAs Omit ----
+iPhone <- na.omit(iPhone)
+
+##### iphoneSentiment: Center and Scale #####
+# Before Centered and Scaled:
+
+# Min.  1st Qu.   Median     Mean  3rd Qu.     Max. 
+# -271.000    0.000    0.000    2.429    0.000  352.000 
+
+#------------------- Center -----------------
+# After Centered:
+
+# Min.      1st Qu.  Median    Mean 3rd Qu.    Max. 
+# -200.00    0.00    0.00    2.43    0.00  200.00
+
+#---- Set max as 200 ----
+for(i in (ncol(iPhone))){
+  iPhone[which(iPhone[,i] >= 200), i] = 200
+}
+#---- Set min as -200 ----
+for(i in (ncol(iPhone))){
+  iPhone[which(iPhone[,i] <= -200), i] = -200
+}
+
+#------------------- Scale -----------------
+# After Scaled:
+
+# Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+# 0.000   5.000   5.000   5.061   5.000  10.000
+
+#---- iphoneSentiment set min as 0 ----
+iPhone$iphoneSentiment <- iPhone$iphoneSentiment+(min(iPhone$iphoneSentiment)*-1)
+
+#---- iphoneSentiment set max as 5 ----
+Scale <- function(x){
+  return(x/(max(x)*0.2)) #<---- divide the value for its maximum (column)
+}
+
+# Apply function
+iPhone$iphoneSentiment <- Scale(iPhone$iphoneSentiment)
+
+#---- iphoneSentiment round intervals (ceiling) for nearest 0.5 ----
+iPhone$iphoneSentiment <- iPhone$iphoneSentiment * 2
+iPhone$iphoneSentiment <- ceiling(iPhone$iphoneSentiment)
+iPhone$iphoneSentiment <- iPhone$iphoneSentiment / 2
+table(iPhone$iphoneSentiment)
+
+##### iphoneSentiment: Remove Noise #####
+
+#---- Positive sentiment: subset ---- 
+iPhonepos <- iPhone %>%
+  filter(iphoneSentiment > 2.5)
+
